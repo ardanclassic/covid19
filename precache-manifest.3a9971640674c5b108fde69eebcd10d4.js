fcda7d5770fc528de8fc1ae3b50a706b2c2059a7
ardanclassic <ardan.classic@gmail.com>
2020-04-08 18:02:29 +0700

    Updates

---
+++ precache-manifest.3a9971640674c5b108fde69eebcd10d4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaad8649f99ba8b5e6cee1a423bfef8e",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "d15463f213d68640ad36",
+    "url": "/covid19/static/css/main.77d5876a.chunk.css"
+  },
+  {
+    "revision": "e5cfe7e7d6bffb41ec51",
+    "url": "/covid19/static/js/2.5902a621.chunk.js"
+  },
+  {
+    "revision": "b87054dff0a800223064988a9ea470df",
+    "url": "/covid19/static/js/2.5902a621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15463f213d68640ad36",
+    "url": "/covid19/static/js/main.f2cbd478.chunk.js"
+  },
+  {
+    "revision": "802b97169c212398fdbc",
+    "url": "/covid19/static/js/runtime-main.9eafc644.js"
+  },
+  {
+    "revision": "79844ee12c5475091184432986399f42",
+    "url": "/covid19/static/media/cnn.79844ee1.png"
+  },
+  {
+    "revision": "b79b6e386819a82db382c9dbb86dd307",
+    "url": "/covid19/static/media/coronavirus.b79b6e38.png"
+  },
+  {
+    "revision": "22ef3391d9c26e0a849c47c0e77d3bfa",
+    "url": "/covid19/static/media/detik.22ef3391.png"
+  },
+  {
+    "revision": "c6baf74de46633578250cbc3b03c5562",
+    "url": "/covid19/static/media/dki.c6baf74d.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/covid19/static/media/github.3e54ed15.png"
+  },
+  {
+    "revision": "c6f6e5c669fa5d7e809fe9cddea4fffe",
+    "url": "/covid19/static/media/ina.c6f6e5c6.png"
+  },
+  {
+    "revision": "2d20662b184fe254365ab651091c9603",
+    "url": "/covid19/static/media/jabar.2d20662b.png"
+  },
+  {
+    "revision": "51371556a884bf1a6a4530f5427104c0",
+    "url": "/covid19/static/media/jatengnew.51371556.png"
+  },
+  {
+    "revision": "d4c56acfb5d53b491b21a53a4e57419c",
+    "url": "/covid19/static/media/jatim.d4c56acf.png"
+  },
+  {
+    "revision": "665445df001a017b06cb6bef68f78f43",
+    "url": "/covid19/static/media/kemenkes.665445df.png"
+  },
+  {
+    "revision": "fe2afa3075934d438b9367e67a8e6d12",
+    "url": "/covid19/static/media/kompas.fe2afa30.png"
+  },
+  {
+    "revision": "580e186a76c8a7ede10408fe7a6e751c",
+    "url": "/covid19/static/media/postman.580e186a.png"
+  },
+  {
+    "revision": "1a2973e4e497b8a9624a8070f5659439",
+    "url": "/covid19/static/media/yogya.1a2973e4.jpg"
+  }
+]);
